c2ef43a7c7e0a3a6d1b781ae52263e90556f8c2b
skkedia <siddhantkedia7@gmail.com>
2022-03-20 11:21:07 +0530

    My first commit

--- src/codechef/Bath_Mar2022.java
+++ src/codechef/Bath_Mar2022.java
@@ -11,20 +11,11 @@ public class Bath_Mar2022 {
 			i--;
 		    Integer t1 = sc.nextInt();
 		    Integer t2 = sc.nextInt();
-		    
 		    sc.nextLine();
-		    
 		    t2 = t2 * 2;
-		    
 		    t2 = t1 / t2;
-		    
 		    System.out.println(t2);
-		    
-		    
 		}
-		
 		sc.close();
-
 	}
-
 }
